cd41fcbdc2ec0680fb1fc3a78f5847fe8dc55c4c
ayumi.kuroda <a.aamcbd@gmail.com>
2021-12-27 12:08:29 +0000

    user login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
+    #Helperに定義していた logged_in? を使用
+    include SessionsHelper 
+    
     include Pagy::Backend
+    
+    #ログイン要求処理
+    def require_user_logged_in
+    unless logged_in?
+      redirect_to login_url
+    end
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,12 @@
 module ApplicationHelper
+    include SessionsHelper
     include Pagy::Frontend
+    
+     private
+
+  def require_user_logged_in
+    unless logged_in?
+      redirect_to login_url
+    end
+  end
 end
