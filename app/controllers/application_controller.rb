678682d4b5da98dd7c3971c028d9d84a7da1b514
ayumi.kuroda <a.aamcbd@gmail.com>
2022-01-04 14:17:06 +0000

    follow unfollow

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,8 +13,10 @@ class ApplicationController < ActionController::Base
       end
     end
    
-    #Micropostの数をカウント
+    #Micropost、フォロー、フォロワーの数をカウント
     def counts(user)
-    @count_microposts = user.microposts.count
-  end
+        @count_microposts = user.microposts.count
+        @count_followings = user.followings.count
+        @count_followers = user.followers.count
+    end
 end
